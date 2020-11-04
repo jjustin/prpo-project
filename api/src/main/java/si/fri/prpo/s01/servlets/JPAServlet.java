4d4b33747478e29fa6b509828002179b1be78b99
Janez Justin <janez.justin0@gmail.com>
2020-11-04 22:25:54 +0100

    feat(servlet): added info to servlet output

--- api/src/main/java/si/fri/prpo/s01/servlets/JPAServlet.java
+++ api/src/main/java/si/fri/prpo/s01/servlets/JPAServlet.java
@@ -2,7 +2,9 @@ package si.fri.prpo.s01.servlets;
 
 import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
 import si.fri.prpo.s01.beans.RoomsBean;
+import si.fri.prpo.s01.beans.StatesBean;
 import si.fri.prpo.s01.entitete.Room;
+import si.fri.prpo.s01.entitete.State;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -11,14 +13,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 
 @WebServlet("/servlet")
 public class JPAServlet extends HttpServlet {
     @Inject
     private RoomsBean roomsBean;
+    @Inject
+    private StatesBean statesBean;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,11 +33,23 @@ public class JPAServlet extends HttpServlet {
 
         // izpis uporabnikov na spletno stran
         writer.println("Rooms:");
-
         for (Room r: rooms){
             writer.println(String.format("- %s", r.getName()));
         }
 
-    }
+        // izpis uporabnikov na spletno stran preko criteriaAPI
+        rooms = roomsBean.getRoomsCriteriaAPI();
+        writer.println("\nRoomsCriteriaAPI:");
+        for (Room r: rooms){
+            writer.println(String.format("- %s", r.getName()));
+        }
 
+        // izpis ene sobe
+        writer.print("\nRoom id=1:\n- ");
+        Room r = roomsBean.getRoom(1);
+        writer.println(r);
+
+        List<State> states = statesBean.getStatesForRoom(1);
+        writer.println("States: " + Arrays.toString(states.toArray()));
+    }
 }
