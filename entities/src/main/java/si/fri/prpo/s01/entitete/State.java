296e763083bdaaf6f02e227c1b2864327331f197
Janez Justin <janez.justin0@gmail.com>
2020-11-04 18:55:30 +0100

    fix(entities): change state timestamp types

--- entities/src/main/java/si/fri/prpo/s01/entitete/State.java
+++ entities/src/main/java/si/fri/prpo/s01/entitete/State.java
@@ -1,6 +1,8 @@
 package si.fri.prpo.s01.entitete;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
 
 @Entity(name = "state")
 @NamedQueries(value =
@@ -16,8 +18,8 @@ public class State {
 
     private Integer number_In; //how many enters
     private Integer number_Out; //how many exits
-    private Integer date;
-    private Integer time;
+    private Date date;
+    private Time time;
 
     @ManyToOne
     @JoinColumn(name = "entrance_id")
@@ -47,19 +49,15 @@ public class State {
         this.number_Out = number_Out;
     }
 
-    public Integer getDate() {
-        return date;
-    }
+    public Date getDate() { return date; }
 
-    public void setDate(Integer date) {
-        this.date = date;
-    }
+    public void setDate(Date date) { this.date = date; }
 
-    public Integer getTime() {
+    public Time getTime() {
         return time;
     }
 
-    public void setTime(Integer time) {
+    public void setTime(Time time) {
         this.time = time;
     }
 
