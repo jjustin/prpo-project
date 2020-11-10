78828ac2f2cb32d08d49c53a5dae4884b493de6e
pikket13 <62064935+pikket13@users.noreply.github.com>
2020-11-10 10:14:17 +0100

    feat(DTO): added roomsDto and stateDto

---
+++ services/src/main/java/si/fri/prpo/s01/dtos/RoomsDTO.java
@@ -0,0 +1,32 @@
+package si.fri.prpo.s01.dtos;
+
+public class RoomsDTO {
+
+    private Integer roomId;
+    private Integer inRoom;
+    private Integer size;
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
+    public Integer getInRoom() {
+        return inRoom;
+    }
+
+    public void setInRoom(Integer inRoom) {
+        this.inRoom = inRoom;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
---
+++ services/src/main/java/si/fri/prpo/s01/dtos/StateDTO.java
@@ -0,0 +1,53 @@
+package si.fri.prpo.s01.dtos;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class StateDTO {
+
+    private Integer stateId;
+    private Integer numberIn;
+    private Integer numberOut;
+    private Date date;
+    private Time time;
+
+    public Integer getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(Integer stateId) {
+        this.stateId = stateId;
+    }
+
+    public Integer getNumberIn() {
+        return numberIn;
+    }
+
+    public void setNumberIn(Integer numberIn) {
+        this.numberIn = numberIn;
+    }
+
+    public Integer getNumberOut() {
+        return numberOut;
+    }
+
+    public void setNumberOut(Integer numberOut) {
+        this.numberOut = numberOut;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+}
