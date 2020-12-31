75c88dfadc1fe1f55033cbf6eed48c72a2e0cb47
Janez Justin <janez.justin0@gmail.com>
2020-12-31 19:24:07 +0100

    feat: cors

--- api/src/main/java/si/fri/prpo/s01/api/v1/RoomCounterApplication.java
+++ api/src/main/java/si/fri/prpo/s01/api/v1/RoomCounterApplication.java
@@ -1,19 +1,16 @@
 package si.fri.prpo.s01.api.v1;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
-import si.fri.prpo.s01.api.v1.mappers.InvalidNumberOfPeopleExceptionMapper;
-import si.fri.prpo.s01.api.v1.sources.EntranceSource;
-import si.fri.prpo.s01.api.v1.sources.RoomSource;
-import si.fri.prpo.s01.api.v1.sources.StateSource;
+
 
 import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
-import java.util.HashSet;
-import java.util.Set;
+
 
 @DeclareRoles({"user", "admin"})
 @OpenAPIDefinition(info = @Info(title = "Room counter API", version = "v1",
@@ -21,5 +18,6 @@ import java.util.Set;
             license = @License(name = "dev"), description = "API for service Room counter"),
             servers = @Server(url = "http://localhost:8080"))
 @ApplicationPath("v1")
+@CrossOrigin(supportedMethods="GET, POST, HEAD, OPTIONS, PUT, DELETE")
 public class RoomCounterApplication extends javax.ws.rs.core.Application {
 }
